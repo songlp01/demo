9fc2fffa895897b93ad3aa27cee092cb5407dd6e
songlp01 <slpling@163.com>
2020-01-03 18:12:13 +0800

    第一次尝试将项目分享到git

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ *
+ */
+@MapperScan("com.example.mapper")
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+}
